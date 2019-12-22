fa19b62143796fe8cdfa6a7b006f7515e98902fa
ivahaprianti <hidayativah@gmail.com>
2019-12-22 09:31:42 +0700

    Tugas13-14

---
+++ 05-Organisasi Program dengan package/src/com/ucup/main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ucup;
+
+/**
+ *
+ * @author Ismail
+ * Nama  : Muhammad Haidzir Ismail
+ * Kelas : TI-1C
+ * Pertemuan 13-14
+ * 
+ */
+public class main {
+    public static void main(String[] args){
+		System.out.println("hello world");
+	}
+}
